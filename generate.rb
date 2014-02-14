544216d012497ccc540593dffa7e527968612032
hiboma <hiroyan@gmail.com>
2014-02-15 02:16:22 +0900

    add: module のひな形作成スクリプトを追加する

---
+++ generate.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+
+mod = ARGV[0] or abort "[usage] #{$0} <module name>"
+
+FileUtils.mkdir_p(mod)
+
+File.write("#{mod}/#{mod}.c", <<-"....")
+#include <linux/module.h>
+
+MODULE_AUTHOR("hiroya");
+MODULE_DESCRIPTION("WARN_ON test");
+MODULE_LICENSE("GPL");
+
+static int __init #{mod}_init(void)
+{
+	return 0;
+}
+
+static void __exit #{mod}_exit(void)
+{
+}
+
+module_init(#{mod}_init);
+module_exit(#{mod}_exit);
+....
+
+File.write("#{mod}/Makefile", <<-"....")
+obj-m := #{mod}.o
+KDIR := /lib/modules/$(shell uname -r)/build
+PWD := $(shell pwd)
+default:
+	$(MAKE) -C $(KDIR) SUBDIRS=$(PWD) modules
+
+insmod:
+	insmod #{mod}.ko
+
+rmmod:
+	rmmod #{mod}.ko
+....
