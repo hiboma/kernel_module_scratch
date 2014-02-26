b5458a5c65be139fd3a6bcfb4f7df54b96822cdd
hiboma <hiroyan@gmail.com>
2014-02-26 10:11:15 +0900

    fix coding style

--- timer/timer.c
+++ timer/timer.c
@@ -8,22 +8,22 @@ static struct timer_list timer;
 
 static void timer_callback(unsigned long __data)
 {
-    pr_info("tick tack\n");
+	pr_info("tick tack\n");
 }
 
 static int __init timer_init(void)
 {
-    init_timer(&timer);
-    timer.expires  = jiffies + 3*HZ; // 3sec
-    timer.data     = 0;
-    timer.function = timer_callback;
-    add_timer(&timer);
+	init_timer(&timer);
+	timer.expires  = jiffies + 3*HZ; // 3sec
+	timer.data     = 0;
+	timer.function = timer_callback;
+	add_timer(&timer);
 	return 0;
 }
 
 static void __exit timer_exit(void)
 {
-    del_timer(&timer);
+	del_timer(&timer);
 }
 
 module_init(timer_init);
