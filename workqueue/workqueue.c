dbb17b2e5b1e489407b60b4d62a9765410c67f73
Hiroya Ito <hiroyan@gmail.com>
2014-04-08 20:37:37 +0900

    fix coding style

--- workqueue/workqueue.c
+++ workqueue/workqueue.c
@@ -10,63 +10,57 @@ static int value;
 static struct dentry *dir_dentry, *file_dentry;
 static struct workqueue_struct *queue;
 
-typedef struct {
-	struct work_struct work;
-} job_t;
+static struct work_struct *job;
+static struct delayed_work *delayed_job;
 
-typedef struct {
-	struct delayed_work work;
-} delayed_job_t;
-
-static job_t *job;
-static delayed_job_t *delayed_job;
-
-// ワーカーのコールバック
-static void callback_function(struct work_struct *work)
+/* ワーカーのコールバック*/
+static void callback(struct work_struct *work)
 {
-	job_t *current_job;
-	current_job = (job_t *)work;
-	printk("[%d] Hello world\n", current->pid);
+	pr_info("[%d] Hello world\n", current->pid);
 }
 
 static ssize_t wq_write(struct file *file, const char __user *buf,
 			 size_t count, loff_t *pos)
 {
-    // 遅延してキューイング
-    if (!queue_delayed_work(queue, (struct delayed_work *)delayed_job, 5 * HZ)) {
-        printk("delayed_job is pending\n");
-        return -EAGAIN;
-    }
+	/* 遅延してキューイング */
+	if (!queue_delayed_work(queue, delayed_job, 5 * HZ)) {
+		pr_info("delayed_job is pending\n");
+		return -EAGAIN;
+	}
 
-    // 遅延無しでキューイング
-    if (!queue_work(queue, (struct work_struct *)job)) {
-        printk("work is pending\n");
-        return -EAGAIN;
-    }
+	/* 遅延無しでキューイング */
+	if (!queue_work(queue, job)) {
+		pr_info("work is pending\n");
+		return -EAGAIN;
+	}
 
-    if (schedule_work((struct work_struct *)job) == 0) {
-	pr_info("schedule_work is pending\n");
-	return -EAGAIN;
-    }
-	
+	if (schedule_work(job) == 0) {
+		pr_info("schedule_work is pending\n");
+		return -EAGAIN;
+	}
+
 	return count;
 }
 
-static struct file_operations fops = {
+static const struct file_operations fops = {
 	.write = wq_write,
 };
 
 static int __init workqueue_init(void)
 {
 	int ret = -ENODEV;
-	
-	/* キューイング用に debugfs でエントリポイントを作る */
+
+	/*
+	 * キューイング用に debugfs で
+	 * エントリポイントを作る
+	 */
 	dir_dentry = debugfs_create_dir("workqueue", NULL);
 	if (!dir_dentry) {
-		printk("failed debugfs_create_dir");
+		pr_info("failed debugfs_create_dir");
 		goto failed;
 	}
-	file_dentry = debugfs_create_file("enqueue", 0666, dir_dentry, &value, &fops);
+	file_dentry = debugfs_create_file("enqueue", 0666,
+					  dir_dentry, &value, &fops);
 
 	/* work_struct 用のキューを作成する */
 	ret = -ENODEV;
@@ -74,17 +68,17 @@ static int __init workqueue_init(void)
 	if (!queue)
 		goto remove_debugfs;
 
-	job = (job_t *)kmalloc(sizeof(job_t), GFP_KERNEL);
+	job = kmalloc(sizeof(struct work_struct *), GFP_KERNEL);
 	if (!job)
 		goto destroy_queue;
 
-	delayed_job = (delayed_job_t *)kmalloc(sizeof(delayed_job_t), GFP_KERNEL);
+	delayed_job = kmalloc(sizeof(struct delayed_work *), GFP_KERNEL);
 	if (!delayed_job)
 		goto free_job;
 
 	/* ワーカーの初期化 */
-	INIT_WORK((struct work_struct *)job, callback_function);
-    INIT_DELAYED_WORK((struct delayed_work *)delayed_job, callback_function);
+	INIT_WORK((struct work_struct *)job, callback);
+	INIT_DELAYED_WORK((struct delayed_work *)delayed_job, callback);
 	return 0;
 
 destroy_queue:
